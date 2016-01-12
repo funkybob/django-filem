cb9ad189198ab8d11cf58dc9349d09854eb85445
Curtis Maloney <curtis@tinbrain.net>
2016-01-12 18:11:37 +1100

    - Rearrange layout to separate content from struture - Add file dropzone - Start writing menus - Genericise file dblclick handling - Wrap tree in root node

--- filem/static/filem/js/base.js
+++ filem/static/filem/js/base.js
@@ -19,9 +19,11 @@ function render_tree(nodelist) {
 }
 
 function render_dir_tree(data) {
-    var tree = document.querySelector('nav');
-    var content = '<ul class="open">';
-    content += render_tree(data.tree, true);
+    var tree = document.querySelector('#tree');
+    var content = '<ul>';
+    content += render_tree([
+        {path: '', name: '/', children: data.tree}
+    ]);
     content += '</ul>';
     tree.innerHTML = content;
     preen_tree();
@@ -44,7 +46,7 @@ function preen_tree() {
 }
 
 function render_file_list(data) {
-    var main = document.querySelector('section');
+    var main = document.querySelector('#files');
     var c = '<ul data-path="' + data.path + '">';
     data.files.forEach(function (node) {
         c += '<li data-name="' + node.name + '" data-type="' + node['content-type'] + '">' + 
@@ -60,7 +62,7 @@ function set_current_path(path) {
     if(path == current_path) { return; }
     history.pushState({}, '', '#' + path);
     current_path = path;
-    document.querySelector('body > header').innerHTML = current_path;
+    document.querySelector('#title span').innerHTML = current_path;
     preen_tree();
     fetch('files/' + path, {credentials: 'same-origin'}).then(json).then(render_file_list);
 }
@@ -68,11 +70,23 @@ function set_current_path(path) {
 $(function () {
     fetch('tree/', {credentials: 'same-origin'}).then(json).then(render_dir_tree);
     set_current_path(document.location.hash.substr(1));
+    $(document).on('click', function (ev) { $('.menu').hide(); });
     $('#tree').on('dblclick', 'li', function (ev) {
         set_current_path(this.dataset['path']);
     });
-    $('#files').on('dblclick', "li[data-type='inode/directory']", function (ev) {
-        set_current_path(this.parentElement.dataset['path'] + '/' + this.dataset['name']);
+    $('#tree').on('contextmenu', 'li', function (ev) {
+        ev.preventDefault();
+        $('#dir-menu').css({
+            display: 'block',
+            top: ev.pageY,
+            left: ev.pageX
+        });
+    });
+    $('#files').on('dblclick', "li", function (ev) {
+        if(this.dataset['type'] == 'inode/directory') {
+            set_current_path(this.parentElement.dataset['path'] + '/' + this.dataset['name']);
+            return;
+        }
     });
     window.onpopstate = function () {
         set_current_path(document.location.hash.substr(1));
